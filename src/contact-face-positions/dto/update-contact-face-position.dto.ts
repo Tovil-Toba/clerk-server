fc8318d5aebdd02e5e9175265b7036e3d177a5fd
Ilya Kondratiev <ikondratev@lanit.ru>
2024-10-31 00:38:14 +0300

    Правка dto обновлений

--- src/contact-face-positions/dto/update-contact-face-position.dto.ts
+++ src/contact-face-positions/dto/update-contact-face-position.dto.ts
@@ -1,4 +1,4 @@
-import { PartialType } from '@nestjs/mapped-types';
+import { PartialType } from '@nestjs/swagger';
 
 import { CreateContactFacePositionDto } from './create-contact-face-position.dto';
 
