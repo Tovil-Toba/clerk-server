d01b7cbe275607c2c474bc26c9ab53cd8ce81449
Ilya Kondratiev <ikondratev@lanit.ru>
2024-10-30 03:34:13 +0300

    CORS, порт вынесен в конфиг

--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,10 @@ import { setupSwagger } from './config/swagger-config';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors();
   setupSwagger(app);
 
-  await app.listen(3000);
+  await app.listen(process.env.PORT ?? 3000);
 }
 
 bootstrap();
